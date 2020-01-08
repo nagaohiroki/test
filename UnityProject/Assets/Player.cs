c0ddf0033c8e7c1a0c5d2573b0d470a2b39ed516
nagaohiroki <testplayers0226@gmail.com>
2020-01-09 08:17:28 +0900

    add ect

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+        
+    }
+}
---
+++ UnityProject/Assets/text.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class text : MonoBehaviour
+{
+	[SerializeField]
+	Text t = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+		t.text = "aa\naa";
+    }
+}
